712649e8f72f887ad04fbd4c4c222d2e4c7ca807
cguzman <christian.guzman@bsc.es>
2022-01-21 11:06:02 +0100

    Merge CMAKE camp main branch: updated data folder with mainGithub branch

---
+++ data/CAMP_v1_paper/binned/test_monarch_binned.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# exit on error
+set -e
+# turn on command echoing
+set -v
+# make sure that the current directory is the one where this script is
+cd ${0%/*}
+# make the output directory if it doesn't exist
+mkdir -p out
+
+((counter = 1))
+while [ true ]
+do
+  echo Attempt $counter
+
+exec_str="../../../camp_v1_paper_binned config_monarch_binned.json interface_monarch_binned.json out/monarch_cb05_soa"
+
+  if ! $exec_str; then
+	  echo Failure "$counter"
+	  if [ "$counter" -gt 0 ]
+	  then
+		  echo FAIL
+		  exit 1
+	  fi
+	  echo retrying...
+  else
+	  echo PASS
+	  exit 0
+  fi
+  ((counter++))
+done
+
