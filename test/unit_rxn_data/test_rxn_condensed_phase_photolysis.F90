7351911439d375a9b36c40fd6988b68aa537a518
Kyle Shores <kyle.shores44@gmail.com>
2023-05-15 10:44:26 -0500

    updating mpi part of the test

--- test/unit_rxn_data/test_rxn_condensed_phase_photolysis.F90
+++ test/unit_rxn_data/test_rxn_condensed_phase_photolysis.F90
@@ -215,12 +215,16 @@ contains
       ! pack the camp core
       pack_size = camp_core%pack_size() &
                   + update_data_GMD%pack_size() &
-                  + update_data_GSD%pack_size()
+                  + update_data_GSD%pack_size() &
+                  + rate_update_A%pack_size() &
+                  + rate_update_B%pack_size()
       allocate(buffer(pack_size))
       pos = 0
       call camp_core%bin_pack(buffer, pos)
       call update_data_GMD%bin_pack(buffer, pos)
       call update_data_GSD%bin_pack(buffer, pos)
+      call rate_update_A%bin_pack(buffer, pos)
+      call rate_update_B%bin_pack(buffer, pos)
       call assert(636035849, pos.eq.pack_size)
     end if
 
@@ -250,12 +254,16 @@ contains
       call camp_core%bin_unpack(buffer, pos)
       call update_data_GMD%bin_unpack(buffer, pos)
       call update_data_GSD%bin_unpack(buffer, pos)
+      call rate_update_A%bin_unpack(buffer, pos)
+      call rate_update_B%bin_unpack(buffer, pos)
       call assert(913246791, pos.eq.pack_size)
       allocate(buffer_copy(pack_size))
       pos = 0
       call camp_core%bin_pack(buffer_copy, pos)
       call update_data_GMD%bin_pack(buffer_copy, pos)
       call update_data_GSD%bin_pack(buffer_copy, pos)
+      call rate_update_A%bin_pack(buffer_copy, pos)
+      call rate_update_B%bin_pack(buffer_copy, pos)
       call assert(408040386, pos.eq.pack_size)
       do i_elem = 1, pack_size
         call assert_msg(185309230, buffer(i_elem).eq.buffer_copy(i_elem), &
