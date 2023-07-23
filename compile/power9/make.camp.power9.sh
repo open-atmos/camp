576910fb9feba87914debfeb409ac9fd7d57c146
cguzman <christian.guzman@bsc.es>
2023-07-23 11:32:49 +0200

    pre-search for watchpoints with gdb (next will be set a print_double for all tempv variable to check when it differs gpu from cpu)

--- compile/power9/make.camp.power9.sh
+++ compile/power9/make.camp.power9.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+make_base(){
 cd /gpfs/scratch/bsc32/bsc32815/a591/nmmb-monarch/MODEL/SRC_LIBS/camp/compile/power9
-./make.camp.power9.sh
+if ! ./make.camp.power9.sh; then
+  exit
+fi
 cd /gpfs/scratch/bsc32/bsc32815/gpupartmc/camp/compile/power9
+}
+#make_base
+#./compile.cvode-3.4-alpha.power9.sh
 
 export SUNDIALS_HOME=$(pwd)/../../../cvode-3.4-alpha/install
 export SUITE_SPARSE_HOME=$(pwd)/../../../SuiteSparse
@@ -73,7 +79,8 @@ else
   if [ "$FILE" == TestMonarch.py ]; then
     log_path="/gpfs/scratch/bsc32/bsc32815/gpupartmc/camp/compile/power9/log.txt"
     echo "Generating log file at " $log_path
-    python $FILE 2>&1 | tee $log_path
+    python $FILE > $log_path
+    #python $FILE 2>&1 | tee $log_path
     #python $FILE
     cd ../../compile/power9
   elif [ "$FILE" == test_monarch_1.py ]; then
