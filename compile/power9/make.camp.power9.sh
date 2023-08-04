83651c92b37cce3ab05b591f0dd4fe6f5e426d31
cguzman <christian.guzman@bsc.es>
2023-08-05 00:35:16 +0200

    pre-debug diff, improved debug

--- compile/power9/make.camp.power9.sh
+++ compile/power9/make.camp.power9.sh
@@ -12,13 +12,18 @@ FILE=TestMonarch.py
 #FILE=./new_make.sh
 if [ "$FILE" == TestMonarch.py ]; then
   #log_path="/gpfs/scratch/bsc32/bsc32815/a591/nmmb-monarch/MODEL/SRC_LIBS/camp/compile/power9/log_gpu.txt"
-  #log_path="../../compile/power9/log_cpu.txt"
+  log_path="../../compile/power9/log_cpu.txt"
   #echo "Generating log file at " $log_path
   #python $FILE > $log_path
-  #python $FILE 2>&1 | tee $log_path
-  python $FILE
+  python $FILE 2>&1 | tee $log_path
+  #python $FILE
+  sed -i 's/conf.caseBase = "CPU One-cell"/conf.caseBase = "GPU BDF"/g' $FILE
+  log_path="../../compile/power9/log_gpu.txt"
+  python $FILE 2>&1 | tee $log_path
+  sed -i 's/conf.caseBase = "GPU BDF"/conf.caseBase = "CPU One-cell"/g' $FILE
   #python translate_netcdf.py
   cd ../../compile/power9
+  diff log_gpu.txt log_cpu.txt 2>&1 | tee diff.txt
 elif [ "$FILE" == test_monarch_1.py ]; then
   echo "Running old commits with file test_monarch_1.py ."
   python  $FILE
