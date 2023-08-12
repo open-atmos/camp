d7f74ab9237f1cde110a3bfba0ae087ad64a82b8
cguzman <christian.guzman@bsc.es>
2023-08-12 17:25:24 +0200

    pre-test EXPORT_F_STATE change to print only rank 0

--- src/debug_and_stats/camp_debug_2.F90
+++ src/debug_and_stats/camp_debug_2.F90
@@ -30,6 +30,27 @@ contains
     print*,comm
     call mpi_comm_rank(comm, rank, ierr)
     call mpi_comm_size(comm, n_ranks, ierr)
+    print*,"mpi_comm_rank",mpi_comm_rank
+    !todo pending gather
+    do k=0,len2-1!camp_core%n_cells
+      if(rank==0) then
+        open(50, file="out/state.csv", status="old", position="append", action="write")
+        do i=1, len!camp_core%size_state_per_cell
+          write(50, "(ES23.15)") x(i+k*len) !camp_state%state_var(i)
+        end do
+        close(50)
+      end if
+    end do
+    print*,"export_f_state",export_f_state
+  end subroutine
+
+  subroutine old_export_f_state(x, len, len2) ! fails in MONARCH
+    real(kind=dp), dimension(:), intent(in) :: x
+    integer, intent(in) :: len, len2
+    integer :: k,j,i
+    print*,comm
+    call mpi_comm_rank(comm, rank, ierr)
+    call mpi_comm_size(comm, n_ranks, ierr)
     do k=0,len2-1!camp_core%n_cells
       do j=0, n_ranks-1
         if(rank==j) then
