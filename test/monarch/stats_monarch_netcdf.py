ac8113a64a71d61a240eda950fada4cc7e5d69c4
cguzman <christian.guzman@bsc.es>
2023-08-20 12:28:13 +0200

    pre-add std and quartile to violinplot

--- test/monarch/stats_monarch_netcdf.py
+++ test/monarch/stats_monarch_netcdf.py
@@ -15,19 +15,13 @@ def process_variable(dataset1, dataset2, var_name):
     # Convert masked arrays to regular NumPy arrays
     array1 = np.ma.getdata(array1)
     array2 = np.ma.getdata(array2)
-    #valid_indices = np.logical_and(~np.isnan(array1), ~np.isnan(array2))
-    #array1 = array1[valid_indices]
-    #array2 = array2[valid_indices]
     abs_diff = abs(abs(array1) - abs(array2))
     relative_error = abs_diff / np.maximum(np.abs(array1), np.abs(array2))  # Calculate relative error
-    # Calculate relative error, replacing NaN values with zeros
-    relative_error = np.where(np.logical_or(np.isnan(array1), np.isnan(array2)), 0.0,
-                              abs_diff / np.maximum(np.abs(array1), np.abs(array2)))
 
     # Calculate statistics
     mean = np.mean(relative_error)
-    if np.isnan(mean): # or mean == 0.
-        #return False
+    if np.isnan(mean) or mean == 0.:
+        return False
         quantiles = np.zeros(4)
         median = 0
         mean = 0
@@ -51,10 +45,10 @@ def main():
     processed_count = 0
     for var_name in variable_names:
         variable = dataset1.variables[var_name]
-        if len(variable.dimensions) == 4 and processed_count < 50:
+        if len(variable.dimensions) == 4 and processed_count < 80:
             result = process_variable(dataset1, dataset2, var_name)
-            #if result:
-            summary_data.append(result)
+            if result:
+                summary_data.append(result)
             processed_count += 1
 
     # Close the NetCDF datasets
@@ -67,19 +61,23 @@ def main():
     numeric_cols = ['Quantiles[25,50,75,95]', 'Median', 'Mean', 'Std Dev']
     summary_table[numeric_cols] = summary_table[numeric_cols].apply(pd.to_numeric, errors='coerce')
 
-    # Find the worst variables based on median relative error
+    # Find the worst variables based on Mean relative error
     worst_variables = summary_table.nlargest(5, 'Mean')  # Modify the number as needed
     #print(worst_variables\n: worst_variables)
 
     # Reshape the DataFrame for plotting
-    worst_variables_melted = pd.melt(worst_variables, id_vars=['Variable'], value_vars=['Median'], var_name='Statistic')
+    worst_variables_melted = pd.melt(worst_variables, id_vars=['Variable'], value_vars=['Mean'], var_name='Statistic')
+
 
     # Create a violin plot for the worst variables
     plt.figure(figsize=(10, 6))
+    #if len(worst_variables_melted['Statistic'].unique()) == 2:  # Check if there are two hue levels
+    #    sns.violinplot(data=worst_variables_melted, x='Variable', y='value', hue='Statistic', inner='quart', palette='Set1')
+    #else:
     sns.violinplot(data=worst_variables_melted, x='Variable', y='value', inner='quart', palette='Set1')
-    plt.title("Violin Plot of Median Relative Error for variables with largest median relative error")
+    plt.title("Violin Plot of Mean Relative Error for variables with largest mean relative error")
     plt.xlabel("Variable")
-    plt.ylabel("Median Relative Error")
+    plt.ylabel("Mean Relative Error")
     plt.xticks(rotation=45)
     plt.tight_layout()
 
