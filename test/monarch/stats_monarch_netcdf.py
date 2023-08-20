0caaae3e7610668149e35b431bc6f4cf147e07ac
cguzman <christian.guzman@bsc.es>
2023-08-20 11:44:38 +0200

    fixed 0 values, failing non int values

--- test/monarch/stats_monarch_netcdf.py
+++ test/monarch/stats_monarch_netcdf.py
@@ -8,15 +8,6 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def calculate_statistics(data):
-    if len(data) == 0:
-        return [0, 0, 0, 0], 0, 0, 0
-    quantiles = np.percentile(data, [25, 50, 75, 95])
-    median = np.median(data)
-    mean = np.mean(data)
-    std_dev = np.std(data)
-    return quantiles, median, mean, std_dev
-
 def process_variable(dataset1, dataset2, var_name):
     print(f"Processing variable: {var_name}")
     array1 = dataset1.variables[var_name][:]
@@ -44,7 +35,7 @@ def main():
     processed_count = 0
     for var_name in variable_names:
         variable = dataset1.variables[var_name]
-        if len(variable.dimensions) == 4 and processed_count < 3:
+        if len(variable.dimensions) == 4 and processed_count < 4:
             result = process_variable(dataset1, dataset2, var_name)
             summary_data.append(result)
             processed_count += 1
@@ -54,29 +45,27 @@ def main():
     dataset2.close()
     # Create a summary table
     summary_table = pd.DataFrame(summary_data, columns=['Variable', 'Quantiles[25,50,75,95]', 'Median', 'Mean', 'Std Dev'])
-    print("\nSummary Table:")
-    print(summary_table)
+    print("Summary Table:",summary_table)
     # Convert statistics columns to numeric data types
     numeric_cols = ['Quantiles[25,50,75,95]', 'Median', 'Mean', 'Std Dev']
     summary_table[numeric_cols] = summary_table[numeric_cols].apply(pd.to_numeric, errors='coerce')
 
-    # Drop rows with NaN values in the numeric columns
-    summary_table.dropna(subset=numeric_cols, inplace=True)
+    # Convert non-numeric values to NaN in the 'Mean' column
+    summary_table['Mean'] = pd.to_numeric(summary_table['Mean'], errors='coerce')
+
+    # Remove rows with NaN in the 'Mean' column
+    summary_table = summary_table.dropna(subset=['Mean'])
 
     # Find the worst variables based on median relative error
-    worst_variables = summary_table.nlargest(2, 'Median')  # Modify the number as needed
-    print(worst_variables)
+    worst_variables = summary_table.nlargest(5, 'Mean')  # Modify the number as needed
+    #print(worst_variables\n: worst_variables)
 
     # Reshape the DataFrame for plotting
     worst_variables_melted = pd.melt(worst_variables, id_vars=['Variable'], value_vars=['Median'], var_name='Statistic')
 
-    # Print the contents of worst_variables_melted for debugging
-    print("worst_variables_melted:")
-    print(worst_variables_melted)
-
     # Create a violin plot for the worst variables
     plt.figure(figsize=(10, 6))
-    sns.violinplot(data=worst_variables_melted, x='Variable', y='value', hue='Statistic', split=True, inner='quart', palette='Set1')
+    sns.violinplot(data=worst_variables_melted, x='Variable', y='value', inner='quart', palette='Set1')
     plt.title("Violin Plot of Median Relative Error for variables with largest median relative error")
     plt.xlabel("Variable")
     plt.ylabel("Median Relative Error")
