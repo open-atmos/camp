ecc95797410efb272cf46b56b3ab6a98a88e0e2a
cguzman <christian.guzman@bsc.es>
2023-09-07 15:43:24 +0200

    pre-rename cvode_ls_gpu.cu

--- src/cuda/cvode_ls_gpu.h
+++ src/cuda/cvode_ls_gpu.h
@@ -3,17 +3,4 @@
  * SPDX-License-Identifier: MIT
  */
 
-#ifndef CVODE_gpu_ls_SOLVER_H_
-#define CVODE_gpu_ls_SOLVER_H_
 
-#include <cuda.h>
-#include "../camp_common.h"
-
-int cvInitialSetup_gpu(CVodeMem cv_mem);
-int cvHin_gpu(CVodeMem cv_mem, realtype tout);
-int cvRcheck1_gpu(CVodeMem cv_mem);
-int cvRcheck2_gpu(CVodeMem cv_mem);
-int cvRcheck3_gpu(CVodeMem cv_mem);
-int cvHandleFailure_gpu(CVodeMem cv_mem, int flag);
-
-#endif
