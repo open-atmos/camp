34a71899ca39f53e158f8aa31a15b45a841d07f7
cguzman <christian.guzman@bsc.es>
2024-04-23 12:13:06 +0200

    prepared bug compiler mn5 with if(true) for support

--- test/unit_rxn_data/test_rxn_CMAQ_H2O2.F90
+++ test/unit_rxn_data/test_rxn_CMAQ_H2O2.F90
@@ -42,6 +42,59 @@ contains
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+  integer function mpi_size( comm )
+    integer, intent(in), optional :: comm
+    integer :: size, ierr, l_comm
+    if (present(comm)) then
+      l_comm = comm
+    else
+      l_comm = MPI_COMM_WORLD
+    endif
+    call mpi_comm_size(l_comm, size, ierr)
+    mpi_size = size
+  end function
+
+  integer function mpi_rank( comm )
+    integer, intent(in), optional :: comm
+    integer :: rank, ierr, l_comm
+    if (present(comm)) then
+      l_comm = comm
+    else
+      l_comm = MPI_COMM_WORLD
+    endif
+    call mpi_comm_rank(l_comm, rank, ierr)
+    mpi_rank = rank
+  end function
+
+  subroutine mpi_transfer_integer(from_val, to_val, from_proc, to_proc)
+    !> Value to send.
+    integer, intent(in) :: from_val
+    !> Variable to send to.
+    integer, intent(out) :: to_val
+    !> Process to send from.
+    integer, intent(in) :: from_proc
+    !> Process to send to.
+    integer, intent(in) :: to_proc
+    integer :: rank, size, ierr, status(MPI_STATUS_SIZE)
+
+    rank = mpi_rank()
+    if (from_proc == to_proc .or. mpi_size() == 1) then
+      if (rank == from_proc) then
+        to_val = from_val
+      end if
+    else
+      if (rank == from_proc) then
+        print*,from_proc,from_val
+        call mpi_send(from_val, 1, MPI_INTEGER, to_proc, &
+            208020430, MPI_COMM_WORLD, ierr)
+      elseif (rank == to_proc) then
+        call mpi_recv(to_val, 1, MPI_INTEGER, from_proc, &
+            208020430, MPI_COMM_WORLD, status, ierr)
+        print*,to_proc,from_val
+      end if
+    end if
+  end subroutine
+
   !> Run all camp_chem_mech_solver tests
   logical function run_CMAQ_H2O2_tests() result(passed)
 
@@ -99,176 +152,6 @@ contains
 
     run_CMAQ_H2O2_test = .true.
 
-    ! Set the rate constants (for calculating the true value)
-    temp = 272.5d0
-    pressure = 101253.3d0
-    air_conc = 1.0d6
-    conv = const%avagadro / const%univ_gas_const * 10.0d0**(-12.0d0) * &
-            pressure / temp
-    k1 = 1.0d0 + air_conc * 4.0e-21 * conv
-    k2 = ( 1476.0d0 * exp(-3.98d2/temp)  * (temp/300.0d0)**(60.0d0) + &
-           air_conc * 4.0d-20 * exp(-2.0d0/temp) *(temp/300.0d0)**(1.5d0) &
-         * conv) / 60.0d0
-
-    ! Set output time step (s)
-    time_step = 1.0
-
-#ifdef CAMP_USE_MPI
-    ! Load the model data on the root process and pass it to process 1 for solving
-    if (camp_mpi_rank().eq.0) then
-#endif
-
-      ! Get the CMAQ_H2O2 reaction mechanism json file
-      input_file_path = 'test_CMAQ_H2O2_config.json'
-
-      ! Construct a camp_core variable
-      camp_core => camp_core_t(input_file_path)
-
-      deallocate(input_file_path)
-
-      ! Initialize the model
-      call camp_core%initialize()
-
-      ! Get the chemical species data
-      call assert(159292767, camp_core%get_chem_spec_data(chem_spec_data))
-
-      ! Get species indices
-      key = "A"
-      idx_A = chem_spec_data%gas_state_id(key);
-      key = "B"
-      idx_B = chem_spec_data%gas_state_id(key);
-      key = "C"
-      idx_C = chem_spec_data%gas_state_id(key);
-
-      ! Make sure the expected species are in the model
-      call assert(715485073, idx_A.gt.0)
-      call assert(545328169, idx_B.gt.0)
-      call assert(375171265, idx_C.gt.0)
-
-#ifdef CAMP_USE_MPI
-      ! pack the camp core
-      pack_size = camp_core%pack_size()
-      allocate(buffer(pack_size))
-      pos = 0
-      call camp_core%bin_pack(buffer, pos)
-      call assert(296902147, pos.eq.pack_size)
-    end if
-
-    ! broadcast the species ids
-    call camp_mpi_bcast_integer(idx_A)
-    call camp_mpi_bcast_integer(idx_B)
-    call camp_mpi_bcast_integer(idx_C)
-
-    ! broadcast the buffer size
-    call camp_mpi_bcast_integer(pack_size)
-
-    if (camp_mpi_rank().eq.1) then
-      ! allocate the buffer to receive data
-      allocate(buffer(pack_size))
-    end if
-
-    ! broadcast the data
-    call camp_mpi_bcast_packed(buffer)
-
-    if (camp_mpi_rank().eq.1) then
-      ! unpack the data
-      camp_core => camp_core_t()
-      pos = 0
-      call camp_core%bin_unpack(buffer, pos)
-      call assert(109951378, pos.eq.pack_size)
-      allocate(buffer_copy(pack_size))
-      pos = 0
-      call camp_core%bin_pack(buffer_copy, pos)
-      call assert(422968304, pos.eq.pack_size)
-      do i_elem = 1, pack_size
-        call assert_msg(159166857, buffer(i_elem).eq.buffer_copy(i_elem), &
-                "Mismatch in element: "//trim(to_string(i_elem)))
-      end do
-
-      ! solve and evaluate results on process 1
-#endif
-
-      ! Initialize the solver
-      call camp_core%solver_initialize()
-
-      ! Get a model state variable
-      camp_state => camp_core%new_state()
-
-      ! Set the environmental conditions
-      call camp_state%env_states(1)%set_temperature_K(   temp )
-      call camp_state%env_states(1)%set_pressure_Pa( pressure )
-
-      ! Save the initial concentrations
-      true_conc(0,idx_A) = 1.0
-      true_conc(0,idx_B) = 0.0
-      true_conc(0,idx_C) = 0.0
-      model_conc(0,:) = true_conc(0,:)
-
-      ! Set the initial concentrations in the model
-      camp_state%state_var(:) = model_conc(0,:)
-
-#ifdef CAMP_DEBUG
-      ! Evaluate the Jacobian during solving
-      solver_stats%eval_Jac = .true.
-#endif
-
-      ! Integrate the mechanism
-      do i_time = 1, NUM_TIME_STEP
-
-        ! Get the modeled conc
-        call camp_core%solve(camp_state, time_step, &
-                              solver_stats = solver_stats)
-        model_conc(i_time,:) = camp_state%state_var(:)
-
-#ifdef CAMP_DEBUG
-        ! Check the Jacobian evaluations
-        call assert_msg(246262196, solver_stats%Jac_eval_fails.eq.0, &
-                        trim( to_string( solver_stats%Jac_eval_fails ) )// &
-                        " Jacobian evaluation failures at time step "// &
-                        trim( to_string( i_time ) ) )
-#endif
-
-        ! Get the analytic conc
-        time = i_time * time_step
-        true_conc(i_time,idx_A) = true_conc(0,idx_A) * exp(-(k1)*time)
-        true_conc(i_time,idx_B) = true_conc(0,idx_A) * (k1/(k2-k1)) * &
-                (exp(-k1*time) - exp(-k2*time))
-        true_conc(i_time,idx_C) = true_conc(0,idx_A) * &
-               (1.0 + (k1*exp(-k2*time) - k2*exp(-k1*time))/(k2-k1))
-
-      end do
-
-      ! Save the results
-      open(unit=7, file="out/CMAQ_H2O2_results.txt", status="replace", &
-              action="write")
-      do i_time = 0, NUM_TIME_STEP
-      write(7,*) i_time*time_step, &
-            ' ', true_conc(i_time, idx_A),' ', model_conc(i_time, idx_A), &
-            ' ', true_conc(i_time, idx_B),' ', model_conc(i_time, idx_B), &
-            ' ', true_conc(i_time, idx_C),' ', model_conc(i_time, idx_C)
-      end do
-      close(7)
-
-      ! Analyze the results
-      do i_time = 1, NUM_TIME_STEP
-        do i_spec = 1, size(model_conc, 2)
-          call assert_msg(911807542, &
-            almost_equal(model_conc(i_time, i_spec), &
-            true_conc(i_time, i_spec), real(1.0e-2, kind=dp)).or. &
-            (model_conc(i_time, i_spec).lt.1e-5*model_conc(1, i_spec).and. &
-            true_conc(i_time, i_spec).lt.1e-5*true_conc(1, i_spec)), &
-            "time: "//trim(to_string(i_time))//"; species: "// &
-            trim(to_string(i_spec))//"; mod: "// &
-            trim(to_string(model_conc(i_time, i_spec)))//"; true: "// &
-            trim(to_string(true_conc(i_time, i_spec))))
-        end do
-      end do
-    deallocate(camp_state)
-
-#ifdef CAMP_USE_MPI
-    end if
-    ! convert the results to an integer
-    !results = 0
     print*,"a"
     if (run_CMAQ_H2O2_test) then
       print*,"run_CMAQ_H2O2_test a"
@@ -277,24 +160,13 @@ contains
       print*,"run_CMAQ_H2O2_test b"
       results = 1
     end if
-    ! Send the results back to the primary process
     !print*,"results",results !if uncommented, works fine
     call camp_mpi_transfer_integer(results, results, 1, 0)
-
-    ! convert the results back to a logical value
-    if (camp_mpi_rank().eq.0) then
-      if (results.eq.0) then
-        run_CMAQ_H2O2_test = .true.
-      else
-        run_CMAQ_H2O2_test = .false.
-      end if
+    if (results.eq.0) then
+      run_CMAQ_H2O2_test = .true.
+    else
+      run_CMAQ_H2O2_test = .false.
     end if
-
-    deallocate(buffer)
-
-#endif
-
-    deallocate(camp_core)
 
   end function run_CMAQ_H2O2_test
 
