3a19df99b8f3d0e6dad0e2c1c39fd4e6bd979101
cguzman <christian.guzman@bsc.es>
2024-10-10 17:14:25 +0200

    doc debug.c

--- src/camp_debug.c
+++ src/camp_debug.c
@@ -15,6 +15,11 @@
 #endif
 #endif
 
+/**
+ * @brief Generates the name of the export state file based on the MPI rank.
+ *
+ * @param filename The array to store the generated filename.
+ */
 void get_export_state_name(char filename[]) {
 #ifdef PROFILE_SOLVING
   int rank;
@@ -27,6 +32,15 @@ void get_export_state_name(char filename[]) {
 #endif
 }
 
+/**
+ * Initializes the export state.
+ *
+ * This function is responsible for initializing the export state. It checks if
+ * the PROFILE_SOLVING macro is defined and if so, it creates a file with the
+ * given filename and closes it immediately. The export state is enabled only
+ * for the process with rank 0.
+ *
+ */
 void init_export_state() {
 #ifdef PROFILE_SOLVING
   char filename[64];
@@ -40,6 +54,16 @@ void init_export_state() {
 #endif
 }
 
+/**
+ * Export the state of the solver.
+ *
+ * This function exports the state of the solver to a file. It is only executed
+ * when the `PROFILE_SOLVING` macro is defined. The state is exported for each
+ * cell in the model data. The exported state variables are stored in the
+ * `total_state` array of the model data.
+ *
+ * @param sd A pointer to the SolverData struct.
+ */
 void export_state(SolverData *sd) {
 #ifdef PROFILE_SOLVING
   ModelData *md = &(sd->model_data);
@@ -58,6 +82,24 @@ void export_state(SolverData *sd) {
 #endif
 }
 
+/**
+ * @brief Joins and exports the state of the program.
+ *
+ * This function is responsible for joining and exporting the state of the
+ * program. It is only executed when the macro PROFILE_SOLVING is defined.
+ *
+ * If the MPI_Comm_size is equal to 1, the function renames the file
+ * "out/0state.csv" to "out/state.csv" and returns.
+ *
+ * If the MPI_Comm_size is greater than 1, the function is executed only by the
+ * process with rank 0. It opens the file "out/state.csv" in write mode and
+ * iterates over all the processes, opening their respective state files
+ * ("out/<rank>state.csv") in read mode. It then reads the contents of each
+ * input file and writes them to the output file. After that, it closes the
+ * input file and removes it. Finally, it closes the output file.
+ *
+ * The function uses MPI_Barrier to synchronize all processes before proceeding.
+ */
 void join_export_state() {
 #ifdef PROFILE_SOLVING
   int size;
@@ -90,6 +132,19 @@ void join_export_state() {
 #endif
 }
 
+/**
+ * Export statistics to a CSV file.
+ *
+ * This function exports statistics related to solving a problem to a CSV file
+ * named "stats.csv". The statistics include the time taken by CVode solver
+ * (`timeCVode`) and the average load balance (`avg_load_balance`). The function
+ * checks if the current process rank is 0 before exporting the statistics. If
+ * the file "stats.csv" does not exist or cannot be opened, an error message is
+ * printed.
+ *
+ * @param sd A pointer to the SolverData structure containing the statistics to
+ * be exported.
+ */
 void export_stats(SolverData *sd) {
 #ifdef PROFILE_SOLVING
   int rank;
--- src/camp_debug.h
+++ src/camp_debug.h
@@ -10,19 +10,12 @@
 
 #include "camp_common.h"
 
-// Create a file for saving output concentrations
 void init_export_state();
-// Export output concentrations to calculate accuracy between CPU and GPU
-// versions at checkGPU test
 void export_state(SolverData *sd);
-// Join the files created by each MPI process at "export_solver_state" function
-// into a single file.
 void join_export_state();
-// Export execution time of GPU and CPU code to calculate speedups at
-// TestMonarch.py
 void export_stats(SolverData *sd);
 
-// file name prefix
+// File name prefix
 int file_name_prefix = 1;
 
 // Maximum size of output file names
