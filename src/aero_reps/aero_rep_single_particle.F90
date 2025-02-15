267c97eb792418e50051b86ac36f3f2fc72db122
Jessica Gasparik <43892206+jtgasparik@users.noreply.github.com>
2025-02-14 21:51:03 -0500

    Update src/aero_reps/aero_rep_single_particle.F90
    Co-authored-by: Matt Dawson <mattldawson@gmail.com>

--- src/aero_reps/aero_rep_single_particle.F90
+++ src/aero_reps/aero_rep_single_particle.F90
@@ -662,25 +662,15 @@ contains
 
     num_phase_instances = 0
     if (present(is_at_surface)) then
-      if (is_at_surface) then
-        do i_layer = 1, NUM_LAYERS_
-          do i_phase = LAYER_PHASE_START_(i_layer), LAYER_PHASE_END_(i_layer)
-            if (this%aero_phase(i_phase)%val%name() .eq. phase_name .and. &
-              this%aero_phase_is_at_surface(i_phase)) then
+      do i_layer = 1, NUM_LAYERS_
+        do i_phase = LAYER_PHASE_START_(i_layer), LAYER_PHASE_END_(i_layer)
+          if (this%aero_phase(i_phase)%val%name() .eq. phase_name .and. &
+              this%aero_phase_is_at_surface(i_phase) .eqv. is_at_surface) then
               num_phase_instances = num_phase_instances + 1
-            end if
-          end do
+          end if
         end do
-      else
-        do i_layer = 1, NUM_LAYERS_
-          do i_phase = LAYER_PHASE_START_(i_layer), LAYER_PHASE_END_(i_layer)
-            if (this%aero_phase(i_phase)%val%name() .eq. phase_name .and. &
-              .not. this%aero_phase_is_at_surface(i_phase)) then
-              num_phase_instances = num_phase_instances + 1
-            end if
-          end do
-        end do
-      end if  
+      end do
+
     else
       do i_layer = 1, NUM_LAYERS_
         do i_phase = LAYER_PHASE_START_(i_layer), LAYER_PHASE_END_(i_layer)
