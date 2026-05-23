5c20262839f8290d4a116d8871bf86825aafa3c2
jtgasparik <jgasparik@bnl.gov>
2026-05-22 23:28:10 -0400

    testing rate constants

--- test/unit_rxn_data/test_rxn_condensed_phase_diffusion.F90
+++ test/unit_rxn_data/test_rxn_condensed_phase_diffusion.F90
@@ -110,6 +110,16 @@ contains
     real(kind=dp), target :: radius, number_conc
     real(kind=dp) :: layer_thickness_l1_p1, layer_thickness_l2_p1, layer_thickness_l3_p1, layer_thickness_l4_p1
     real(kind=dp) :: surface_area_l2, surface_area_l3, surface_area_l4
+    real(kind=dp) :: volume_phase_l1_p1, volume_phase_l2_p1
+    real(kind=dp) :: rate_second, expected_rate_first, expected_rate_second
+    real(kind=dp) :: test_tolerance
+    ! Additional test for layer pair 2-3 (between layer 2 and layer 3)
+    real(kind=dp) :: volume_phase_l3_p1
+    real(kind=dp) :: rate_first_l2l3, rate_second_l2l3
+    real(kind=dp) :: expected_rate_first_l2l3, expected_rate_second_l2l3
+    real(kind=dp) :: volume_phase_l3_p1_final, volume_phase_l4_p1
+    real(kind=dp) :: rate_first_l3l4, rate_second_l3l4
+    real(kind=dp) :: expected_rate_first_l3l4, expected_rate_second_l3l4
 
     integer(kind=i_kind) :: i_sect_unused, i_sect_the_mode
 
@@ -507,9 +517,10 @@ contains
       print *, "Calculated layer thickness 1 (m): ", layer_thickness_l1_p1
       print *, "Calculated layer thickness 2 (m): ", layer_thickness_l2_p1
       print *, "Calculated layer thickness 3 (m): ", layer_thickness_l3_p1
-      print *, "Calculated surface area (m2): ", surface_area_l2
-      print *, "Calculated surface area (m2): ", surface_area_l3
-      print *, "Calculated surface area (m2): ", surface_area_l4
+      print *, "Calculated layer thickness 4 (m): ", layer_thickness_l4_p1
+      print *, "Calculated surface area l2 (m2): ", surface_area_l2
+      print *, "Calculated surface area l3 (m2): ", surface_area_l3
+      print *, "Calculated surface area l4 (m2): ", surface_area_l4
 
       ! Update the aerosol representation (single particle only)
       call number_update%set_number__n_m3(1, number_conc)
@@ -535,6 +546,10 @@ contains
         !  print *, "state_var after step", i_time, ":", camp_state%state_var(13:min(44, size(camp_state%state_var)))
         !end if
       end do
+      print *, "model_conc at all time steps for solute in layer 1: ", model_conc(:,idx_solute_l1_p1)
+      print *, "model_conc at all time steps for solute in layer 2: ", model_conc(:,idx_solute_l2_p1)
+      print *, "model_conc at all time steps for solute in layer 3: ", model_conc(:,idx_solute_l3_p1)
+      print *, "model_conc at all time steps for solute in layer 4: ", model_conc(:,idx_solute_l4_p1)
 
 #ifdef CAMP_DEBUG
         ! Check the Jacobian evaluations
@@ -579,11 +594,200 @@ contains
                                 trim(to_string(diff_coeff_second(i)))//" expected "// &
                                 trim(to_string(expected_diff_coeff)))
               end do
-              rate_first = -((surface_area_l2 * diff_coeff_first(1)) / (layer_thickness_l1_p1 * (true_conc(0,idx_solute_l1_p1) + true_conc(0,idx_H2O_l1_p1)))) * &
-                            true_conc(0,idx_solute_l1_p1) + &
-                            ((surface_area_l2 * diff_coeff_second(1)) / (layer_thickness_l2_p1 * (true_conc(0,idx_solute_l2_p1) + true_conc(0,idx_H2O_l2_p1)))) * &
-                            true_conc(0,idx_solute_l2_p1)
-              print *, "Calculated rate first layer: ", rate_first
+              ! Test rate_first and rate_second calculations based on surface_area and layer_thickness
+              ! 
+              ! Formula from condensed phase diffusion solver:
+              ! rate_first = (eff_sa / volume_phase_first) * (
+              !     (-DIFF_COEFF_FIRST_ / layer_thickness_first) * state[PHASE_ID_FIRST_] +
+              !     (DIFF_COEFF_SECOND_ / layer_thickness_second) * state[PHASE_ID_SECOND_]
+              ! )
+              !
+              ! rate_second = (eff_sa / volume_phase_second) * (
+              !     (DIFF_COEFF_FIRST_ / layer_thickness_first) * state[PHASE_ID_FIRST_] -
+              !     (DIFF_COEFF_SECOND_ / layer_thickness_second) * state[PHASE_ID_SECOND_]
+              ! )
+              !
+              ! Where:
+              ! - eff_sa = interface surface area between layers (m2)
+              ! - volume_phase_first = total mass of first phase (kg per particle)
+              ! - volume_phase_second = total mass of second phase (kg per particle)
+              ! - state values = concentrations of species in each layer (kg/m3 per particle)
+              
+              test_tolerance = 1.0d-12
+              
+              ! Calculate volume (mass) of each phase
+              volume_phase_l1_p1 = true_conc(0,idx_solute_l1_p1) + true_conc(0,idx_H2O_l1_p1)
+              print *, "Calculated volume_phase_l1_p1 (kg per particle): ", volume_phase_l1_p1
+              volume_phase_l2_p1 = true_conc(0,idx_solute_l2_p1) + true_conc(0,idx_H2O_l2_p1)
+              print *, "Calculated volume_phase_l2_p1 (kg per particle): ", volume_phase_l2_p1
+
+              ! Calculate expected rate_first
+              ! rate_first = (eff_sa / volume_phase_first) * (
+              !     (-DIFF_COEFF_FIRST_ / layer_thickness_first) * state_l1 +
+              !     (DIFF_COEFF_SECOND_ / layer_thickness_second) * state_l2
+              ! )
+              expected_rate_first = (surface_area_l2 / volume_phase_l1_p1) * ( &
+                  (-diff_coeff_first(1) / layer_thickness_l1_p1) * true_conc(0,idx_solute_l1_p1) + &
+                  (diff_coeff_second(1) / layer_thickness_l2_p1) * true_conc(0,idx_solute_l2_p1) )
+              
+              ! Calculate expected rate_second
+              ! rate_second = (eff_sa / volume_phase_second) * (
+              !     (DIFF_COEFF_FIRST_ / layer_thickness_first) * state_l1 -
+              !     (DIFF_COEFF_SECOND_ / layer_thickness_second) * state_l2
+              ! )
+              expected_rate_second = (surface_area_l2 / volume_phase_l2_p1) * ( &
+                  (diff_coeff_first(1) / layer_thickness_l1_p1) * true_conc(0,idx_solute_l1_p1) - &
+                  (diff_coeff_second(1) / layer_thickness_l2_p1) * true_conc(0,idx_solute_l2_p1) )
+              
+              ! Calculate actual rate_first using the diffusion formula
+              rate_first = (surface_area_l2 / volume_phase_l1_p1) * ( &
+                  (-diff_coeff_first(1) / layer_thickness_l1_p1) * true_conc(0,idx_solute_l1_p1) + &
+                  (diff_coeff_second(1) / layer_thickness_l2_p1) * true_conc(0,idx_solute_l2_p1) )
+              
+              ! Calculate actual rate_second
+              rate_second = (surface_area_l2 / volume_phase_l2_p1) * ( &
+                  (diff_coeff_first(1) / layer_thickness_l1_p1) * true_conc(0,idx_solute_l1_p1) - &
+                  (diff_coeff_second(1) / layer_thickness_l2_p1) * true_conc(0,idx_solute_l2_p1) )
+              
+              ! Test rate_first calculation
+              call assert_msg(782934561, almost_equal(rate_first, expected_rate_first, test_tolerance), &
+                  "rate_first calculation failed. " // &
+                  "Calculated: " // trim(to_string(rate_first)) // &
+                  ", Expected: " // trim(to_string(expected_rate_first)) // &
+                  ", surface_area_l2: " // trim(to_string(surface_area_l2)) // &
+                  ", volume_phase_l1_p1: " // trim(to_string(volume_phase_l1_p1)) // &
+                  ", layer_thickness_l1_p1: " // trim(to_string(layer_thickness_l1_p1)) // &
+                  ", layer_thickness_l2_p1: " // trim(to_string(layer_thickness_l2_p1)) // &
+                  ", diff_coeff_first(1): " // trim(to_string(diff_coeff_first(1))) // &
+                  ", diff_coeff_second(1): " // trim(to_string(diff_coeff_second(1))))
+              
+              ! Test rate_second calculation
+              call assert_msg(193847265, almost_equal(rate_second, expected_rate_second, test_tolerance), &
+                  "rate_second calculation failed. " // &
+                  "Calculated: " // trim(to_string(rate_second)) // &
+                  ", Expected: " // trim(to_string(expected_rate_second)) // &
+                  ", surface_area_l2: " // trim(to_string(surface_area_l2)) // &
+                  ", volume_phase_l2_p1: " // trim(to_string(volume_phase_l2_p1)) // &
+                  ", layer_thickness_l1_p1: " // trim(to_string(layer_thickness_l1_p1)) // &
+                  ", layer_thickness_l2_p1: " // trim(to_string(layer_thickness_l2_p1)) // &
+                  ", diff_coeff_first(1): " // trim(to_string(diff_coeff_first(1))) // &
+                  ", diff_coeff_second(1): " // trim(to_string(diff_coeff_second(1))))
+              
+              ! Test that rate_first and rate_second have opposite signs (conservation of mass)
+              call assert_msg(457382910, &
+                  (rate_first > 0.0_dp .and. rate_second < 0.0_dp) .or. &
+                  (rate_first < 0.0_dp .and. rate_second > 0.0_dp) .or. &
+                  (rate_first == 0.0_dp .and. rate_second == 0.0_dp), &
+                  "rate_first and rate_second should have opposite signs (mass conservation). " // &
+                  "rate_first: " // trim(to_string(rate_first)) // &
+                  ", rate_second: " // trim(to_string(rate_second)))
+              
+              print *, "Calculated rate_first: ", rate_first
+              print *, "Expected rate_first: ", expected_rate_first
+              print *, "Calculated rate_second: ", rate_second
+              print *, "Expected rate_second: ", expected_rate_second
+              
+              ! Additional test for layer pair 2-3 (between layer 2 and layer 3)
+              
+              volume_phase_l3_p1 = true_conc(0,idx_solute_l3_p1) + true_conc(0,idx_H2O_l3_p1)
+              print *, "Calculated volume_phase_l3_p1 (kg per particle): ", volume_phase_l3_p1
+              volume_phase_l2_p1 = true_conc(0,idx_solute_l2_p1) + true_conc(0,idx_H2O_l2_p1)
+              print *, "Calculated volume_phase_l2_p1 (kg per particle): ", volume_phase_l2_p1
+              
+              ! Calculate expected rates for layer pair 2-3
+              expected_rate_first_l2l3 = (surface_area_l3 / volume_phase_l2_p1) * ( &
+                  (-diff_coeff_first(1) / layer_thickness_l2_p1) * true_conc(0,idx_solute_l2_p1) + &
+                  (diff_coeff_second(1) / layer_thickness_l3_p1) * true_conc(0,idx_solute_l3_p1) )
+              
+              expected_rate_second_l2l3 = (surface_area_l3 / volume_phase_l3_p1) * ( &
+                  (diff_coeff_first(1) / layer_thickness_l2_p1) * true_conc(0,idx_solute_l2_p1) - &
+                  (diff_coeff_second(1) / layer_thickness_l3_p1) * true_conc(0,idx_solute_l3_p1) )
+              
+              ! Calculate actual rates for layer pair 2-3
+              rate_first_l2l3 = (surface_area_l3 / volume_phase_l2_p1) * ( &
+                  (-diff_coeff_first(1) / layer_thickness_l2_p1) * true_conc(0,idx_solute_l2_p1) + &
+                  (diff_coeff_second(1) / layer_thickness_l3_p1) * true_conc(0,idx_solute_l3_p1) )
+              
+              rate_second_l2l3 = (surface_area_l3 / volume_phase_l3_p1) * ( &
+                  (diff_coeff_first(1) / layer_thickness_l2_p1) * true_conc(0,idx_solute_l2_p1) - &
+                  (diff_coeff_second(1) / layer_thickness_l3_p1) * true_conc(0,idx_solute_l3_p1) )
+              
+              ! Test rate calculations for layer pair 2-3
+              call assert_msg(847263918, almost_equal(rate_first_l2l3, expected_rate_first_l2l3, test_tolerance), &
+                  "rate_first layer 2-3 calculation failed. " // &
+                  "Calculated: " // trim(to_string(rate_first_l2l3)) // &
+                  ", Expected: " // trim(to_string(expected_rate_first_l2l3)))
+              
+              call assert_msg(156274839, almost_equal(rate_second_l2l3, expected_rate_second_l2l3, test_tolerance), &
+                  "rate_second layer 2-3 calculation failed. " // &
+                  "Calculated: " // trim(to_string(rate_second_l2l3)) // &
+                  ", Expected: " // trim(to_string(expected_rate_second_l2l3)))
+              
+              ! Verify mass conservation for layer pair 2-3
+              call assert_msg(758301627, &
+                  (rate_first_l2l3 > 0.0_dp .and. rate_second_l2l3 < 0.0_dp) .or. &
+                  (rate_first_l2l3 < 0.0_dp .and. rate_second_l2l3 > 0.0_dp) .or. &
+                  (rate_first_l2l3 == 0.0_dp .and. rate_second_l2l3 == 0.0_dp), &
+                  "rate_first_l2l3 and rate_second_l2l3 should have opposite signs. " // &
+                  "rate_first_l2l3: " // trim(to_string(rate_first_l2l3)) // &
+                  ", rate_second_l2l3: " // trim(to_string(rate_second_l2l3)))
+              
+              print *, "Layer 2-3 tests passed"
+              print *, "Calculated rate_first (l2-l3): ", rate_first_l2l3
+              print *, "Expected rate_first (l2-l3): ", expected_rate_first_l2l3
+              print *, "Calculated rate_second (l2-l3): ", rate_second_l2l3
+              print *, "Expected rate_second (l2-l3): ", expected_rate_second_l2l3
+              
+              ! Additional test for layer pair 3-4 (between layer 3 and layer 4)
+              
+              volume_phase_l3_p1_final = true_conc(0,idx_solute_l3_p1) + true_conc(0,idx_H2O_l3_p1)
+              volume_phase_l4_p1 = true_conc(0,idx_solute_l4_p1) + true_conc(0,idx_H2O_l4_p1)
+              
+              ! Calculate expected rates for layer pair 3-4
+              expected_rate_first_l3l4 = (surface_area_l4 / volume_phase_l3_p1_final) * ( &
+                  (-diff_coeff_first(1) / layer_thickness_l3_p1) * true_conc(0,idx_solute_l3_p1) + &
+                  (diff_coeff_second(1) / layer_thickness_l4_p1) * true_conc(0,idx_solute_l4_p1) )
+              
+              expected_rate_second_l3l4 = (surface_area_l4 / volume_phase_l4_p1) * ( &
+                  (diff_coeff_first(1) / layer_thickness_l3_p1) * true_conc(0,idx_solute_l3_p1) - &
+                  (diff_coeff_second(1) / layer_thickness_l4_p1) * true_conc(0,idx_solute_l4_p1) )
+              
+              ! Calculate actual rates for layer pair 3-4
+              rate_first_l3l4 = (surface_area_l4 / volume_phase_l3_p1_final) * ( &
+                  (-diff_coeff_first(1) / layer_thickness_l3_p1) * true_conc(0,idx_solute_l3_p1) + &
+                  (diff_coeff_second(1) / layer_thickness_l4_p1) * true_conc(0,idx_solute_l4_p1) )
+              
+              rate_second_l3l4 = (surface_area_l4 / volume_phase_l4_p1) * ( &
+                  (diff_coeff_first(1) / layer_thickness_l3_p1) * true_conc(0,idx_solute_l3_p1) - &
+                  (diff_coeff_second(1) / layer_thickness_l4_p1) * true_conc(0,idx_solute_l4_p1) )
+              
+              ! Test rate calculations for layer pair 3-4
+              call assert_msg(637194856, almost_equal(rate_first_l3l4, expected_rate_first_l3l4, test_tolerance), &
+                  "rate_first layer 3-4 calculation failed. " // &
+                  "Calculated: " // trim(to_string(rate_first_l3l4)) // &
+                  ", Expected: " // trim(to_string(expected_rate_first_l3l4)))
+              
+              call assert_msg(264857193, almost_equal(rate_second_l3l4, expected_rate_second_l3l4, test_tolerance), &
+                  "rate_second layer 3-4 calculation failed. " // &
+                  "Calculated: " // trim(to_string(rate_second_l3l4)) // &
+                  ", Expected: " // trim(to_string(expected_rate_second_l3l4)))
+              
+              ! Verify mass conservation for layer pair 3-4
+              call assert_msg(849374621, &
+                  (rate_first_l3l4 > 0.0_dp .and. rate_second_l3l4 < 0.0_dp) .or. &
+                  (rate_first_l3l4 < 0.0_dp .and. rate_second_l3l4 > 0.0_dp) .or. &
+                  (rate_first_l3l4 == 0.0_dp .and. rate_second_l3l4 == 0.0_dp), &
+                  "rate_first_l3l4 and rate_second_l3l4 should have opposite signs. " // &
+                  "rate_first_l3l4: " // trim(to_string(rate_first_l3l4)) // &
+                  ", rate_second_l3l4: " // trim(to_string(rate_second_l3l4)))
+              
+              print *, "Layer 3-4 tests passed"
+              print *, "Calculated rate_first (l3-l4): ", rate_first_l3l4
+              print *, "Expected rate_first (l3-l4): ", expected_rate_first_l3l4
+              print *, "Calculated rate_second (l3-l4): ", rate_second_l3l4
+              print *, "Expected rate_second (l3-l4): ", expected_rate_second_l3l4
+              print *, ""
+              print *, "All rate_first and rate_second tests PASSED"
           end select
         end do
       end if
