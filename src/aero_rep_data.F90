0b4e5a671bf2e59bc1a36f6909d6255498bf1735
jtgasparik <jgasparik@bnl.gov>
2026-06-14 23:00:03 -0400

    fix to Doxygen configuration documentation

--- src/aero_rep_data.F90
+++ src/aero_rep_data.F90
@@ -392,6 +392,10 @@ interface
 
   !> Determine if specified phase(s) exist in adjacent layers. Returns array
   !! of phase_ids for adjacent phases first and second.
+  !! @param this Aerosol representation data
+  !! @param phase_name_first First phase name
+  !! @param phase_name_second Second phase name
+  !! @return Array of index pairs for adjacent phases
 
   function adjacent_phases(this, phase_name_first, &
                            phase_name_second) result (index_pairs)
@@ -406,14 +410,17 @@ interface
     character(len=*), intent(in) :: phase_name_first
     character(len=*), intent(in) :: phase_name_second
 
-
     end function adjacent_phases
   
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-    !> Get the species id on the state array by phase_id and species name
+  !> Get the species id on the state array by phase_id and species name
+  !! @param this Aerosol representation data
+  !! @param phase_id Phase id
+  !! @param spec_name Species name
+  !! @return Species state id
 
-    function spec_state_id_by_phase(this, phase_id, spec_name) result(spec_id)
+  function spec_state_id_by_phase(this, phase_id, spec_name) result(spec_id)
       use camp_util,                          only : i_kind, integer_to_string
       import :: aero_rep_data_t
 
